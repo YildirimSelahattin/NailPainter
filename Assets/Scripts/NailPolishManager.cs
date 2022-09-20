062ecc023d1f06e3d6563129f8d069069f1098eb
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2022-09-20 14:30:22 +0300

    Test

--- Assets/Scripts/NailPolishManager.cs
+++ Assets/Scripts/NailPolishManager.cs
@@ -11,28 +11,36 @@ public class NailPolishManager : MonoBehaviour
     [SerializeField] GameObject Nail3;
     [SerializeField] GameObject Nail4;
     [SerializeField] GameObject Nail5;
-    [SerializeField] Texture[] myTexture;
+    [SerializeField] Texture[] myColor;
+    [SerializeField] Texture[] myPattern;
     int colorIndex;
+    public Material test;
 
     void Start()
     {
         //Find the Standard Shader
-        Material myNewMaterial = new Material(Shader.Find("Standard"));
+        Material myNewColorMaterial = new Material(Shader.Find("Standard"));
         //Set Texture on the material
-        myNewMaterial.SetTexture("_MainTex", myTexture[colorIndex]);
+        myNewColorMaterial.SetTexture("_MainTex", myColor[colorIndex]);
+        //Find the Standard Shader
+        Material myNewPatternMaterial = new Material(Shader.Find("Transparent/Diffuse"));
+        //Set Texture on the material
+        myNewPatternMaterial.SetTexture("_MainTex", myPattern[0]);
 
         //if(Collision)
-        Brush.GetComponent<MeshRenderer>().material = myNewMaterial;
-        Nail1.GetComponent<MeshRenderer>().material = myNewMaterial;
-        Nail2.GetComponent<MeshRenderer>().material = myNewMaterial;
-        Nail3.GetComponent<MeshRenderer>().material = myNewMaterial;
-        Nail4.GetComponent<MeshRenderer>().material = myNewMaterial;
-        Nail5.GetComponent<MeshRenderer>().material = myNewMaterial;
+        Brush.GetComponent<MeshRenderer>().material = myNewColorMaterial;
+        Brush.GetComponent<MeshRenderer>().materials[0] = test;
+
+        Nail1.GetComponent<MeshRenderer>().material = myNewColorMaterial;
+        Nail2.GetComponent<MeshRenderer>().material = myNewColorMaterial;
+        Nail3.GetComponent<MeshRenderer>().material = myNewColorMaterial;
+        Nail4.GetComponent<MeshRenderer>().material = myNewColorMaterial;
+        Nail5.GetComponent<MeshRenderer>().material = myNewColorMaterial;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Brush.GetComponent<MeshRenderer>().materials[1] = test;
     }
 }
