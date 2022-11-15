ac65b23dad79e0eabdf0eed1b7cf5b7f1c7d92d0
emirYarici <70836855+emirYarici@users.noreply.github.com>
2022-11-15 16:46:19 +0300

    mud tool blueprint added and dust material surface type changed

---
+++ Assets/Scripts/MachineScripts/MudMachineManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudMachineManager : MonoBehaviour
+{
+    [SerializeField] Material MudMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.transform.CompareTag("Hand"))
+        {
+            Material[] matArray = other.gameObject.GetComponent<MeshRenderer>().materials;
+            matArray[1] = MudMaterial;
+            other.gameObject.GetComponent<MeshRenderer>().materials = matArray;
+        }
+    }
+}
