f936c65600e25894e6a91ee392eb168fbe373723
emirYarici <e-yarici@hotmail.com>
2022-12-06 11:24:24 +0300

    Revert "Revert "Revert "level fixes"""
    This reverts commit 5a59ea3fa9caf58122bbf67fdaa968cdefbbfdbe.

--- Assets/Scripts/MovingPolishManager.cs
+++ Assets/Scripts/MovingPolishManager.cs
@@ -27,8 +27,8 @@ public class MovingPolishManager : MonoBehaviour
         int index = ((int)GameManager.Instance.matchRate / 5) - 1;
         endPos = colorableObjectsParent.transform.GetChild(index).transform.position;
         journeyLength = Vector3.Distance(startPos, endPos);
-        
-        camera.transform.DOLocalMove(targetCameraPosition, 1f).OnComplete(() => transform.DOLocalMove(new Vector3(endPos.x + 2,endPos.y,endPos.z), 10).OnComplete(() => CloseBrushAndOpenRewarPanel()));
+
+        camera.transform.DOLocalMove(targetCameraPosition, 1f).OnComplete(() => transform.DOLocalMove(new Vector3(endPos.x,endPos.y+1,endPos.z), 10).OnComplete(() => CloseBrushAndOpenRewarPanel()));
         camera.transform.DOLocalRotate(targetCameraRotation, 1f);
         transform.DOLocalRotate(new Vector3(-30, 180, -90f), 0.1f);
     }
